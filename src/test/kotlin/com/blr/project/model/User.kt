7d6a0cbe18d590cdb371edc49f4a6f5c16cf94eb
Aliaksandr Sheikin <aliaksandr_sheikin@epam.com>
2018-04-09 20:31:10 +0300

    Fixed problem with Kotlin data class

--- src/test/kotlin/com/blr/project/model/User.kt
+++ src/test/kotlin/com/blr/project/model/User.kt
@@ -3,8 +3,11 @@ package com.blr.project.model
 import com.blr.project.utils.RandomUtils.randomString
 
 //@Component
-data class User(var name: String = randomString("test#name"),
-                var job: String = randomString("test#job"),
+data class User constructor(var name: String? = null,
+                var job: String? = null,
                 var id: String? = null,
                 var createdAt: String? = null,
-                var updatedAt: String? = null)
+                var updatedAt: String? = null) {
+
+    constructor() : this(randomString("test#name"), randomString("test#job"), null, null)
+}
